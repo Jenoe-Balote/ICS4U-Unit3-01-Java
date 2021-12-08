e5d5771977d9144957c07a598e9f63ee6c495076
Ubuntu <ubuntu@ip-172-31-88-106.ec2.internal>
2021-12-08 18:00:06 +0000

    Added Main.java and MrCoxallStack.java

---
+++ Main.java
@@ -0,0 +1,39 @@
+/*
+ * This program pushes and prints a stack.
+ *
+ * @author Jenoe Balote
+ * @version 1.0
+ * @since 2021-12-07
+ */
+
+/**
+ * This class also thing.
+ * */
+final class Main {
+
+    /**
+     * Prevents instantiation.
+     * Throw an exception IllegalStateException when called.
+     *
+     * @throws IllegalStateException
+     *
+     */
+    private Main() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+     * This class also thing.
+     *
+     * @param args no args will be used
+     *
+     * */
+    public static void main(final String[] args) {
+        final int number1 = 123;
+        final MrCoxallStack aStack = new MrCoxallStack();
+
+        aStack.push(number1);
+        aStack.showStack();
+    }
+}
+
